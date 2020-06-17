38131cf1afa00340292b57954190d15ae42ccb00
UnknowEntity <theplayer357@gmail.com>
2020-06-18 04:54:56 +0700

    change

--- server.js
+++ server.js
@@ -120,7 +120,7 @@ app.post("/request-list", (req, res) => {
 
 app.post("/setelection", (req, res) => {
   const { year, name, nominees, deadline } = req.body;
-  io.emit(actions.ADD_ELECTION, req.body);
+  //io.emit(actions.ADD_ELECTION, req.body);
   blockChain.setElection(year, name, nominees, deadline);
   res.json({ status: 200 });
 });
