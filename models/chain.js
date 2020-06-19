93f1ff0124c1e9e15b72c5aee488be4dd1424858
UnknowEntity <theplayer357@gmail.com>
2020-06-19 22:26:58 +0700

    wrong type bug

--- models/chain.js
+++ models/chain.js
@@ -278,7 +278,7 @@ class Chain {
     for (let index = 0; index < this.blocks.length; index++) {
       let currentBlockActions = this.blocks[index].actions;
       for (let index1 = 0; index1 < currentBlockActions.length; index1++) {
-        if (currentBlockActions[index1].type === "user") {
+        if (currentBlockActions[index1].type === "users") {
           if (currentBlockActions[index1].id === lock) {
             let tempAction = new Action(null, null, null, null);
             tempAction.parseData(currentBlockActions[index1]);
